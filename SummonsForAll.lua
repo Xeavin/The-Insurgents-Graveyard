715ee493913b33eee76f661cd4384045669f3ed8
Xeavin <lussios@protonmail.com>
2021-06-14 08:51:57 +0200

    Update v1.1.0
    - Added a "Summons For All" option.
    - No Restoration On Save Crystals: Fixed an issue where cutscenes that initiated a boss fight wouldn't restore any HP, MP, etc.
    - No Restoration On Save Crystals: Fixed an issue where certain cutscenes could trigger a crash.

--- SummonsForAll.lua
+++ SummonsForAll.lua
@@ -1,24 +1,23 @@
 --Made by Xeavin
-local codePointer = 0x00356500
-local originalBytes = {0x48, 0x83, 0xEC, 0x28}
+local codePointer = 0x00323E33
+local originalBytes = {0x85, 0xC0}
 local code =
 [[
-  ret
-  nop 0x03
+  cmp eax,eax
 ]]
 
-print("No Heal On Save Crystals (NHOSC): Applying patch.")
+print("Summons For All (SFA): Applying patch.")
 local readBytes = memory.readArray(codePointer, #originalBytes)
 if (#readBytes ~= #originalBytes) then
-  print("NHOSC: Couldn't read from memory.")
+  print("SFA: Couldn't read from memory.")
   return
 elseif (table.concat(readBytes) ~= table.concat(originalBytes)) then
-  print("NHOSC: Unexpected values, aborting.")
+  print("SFA: Unexpected values, aborting.")
   return
 end
 
 if not (memory.assemble(code, codePointer)) then
-  print("NHOSC: Couldn't write to memory.")
+  print("SFA: Couldn't write to memory.")
   return
 end
 
