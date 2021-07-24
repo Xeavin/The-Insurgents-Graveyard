32890fc7d2a23d8695c598d6ea8e2cd6fc7aaaa9
Xeavin <lussios@protonmail.com>
2021-07-24 18:08:46 +0200

    Update v1.2.0
    - All scripts are now also available as a cheat table.
    - Added support for the Microsoft Store version of the game (cheat table only).
    - No Overlay Map: Is now actually disabled where as before only its toggle was.
    No Speed Mode: Is now actually disabled where as before only its toggle was.
    - Camera Position: Removed because it is incompatible with The Insurgent's Vision, whose Free Camera option is superior to this script anyway.
    - Cleaned up a few scripts.

--- LUA Scripts/NoTeleportRequirement.lua
+++ LUA Scripts/NoTeleportRequirement.lua
@@ -1,6 +1,6 @@
 --Made by Xeavin
 local codePointer = 0x0032A860
-local originalBytes = {0x48, 0x83, 0xEC, 0x28, 0x33, 0xD2, 0xE8, 0x45, 0xF7, 0xFD, 0xFF}
+local originalBytes = {0x48, 0x83, 0xEC, 0x28, 0x33, 0xD2}
 local code =
 [[
   jmp %ntr_code%
@@ -14,14 +14,14 @@ ntr_code:
   jne short ntr_originalcode
   mov eax,0x00000063
   ret
-  
+
 ntr_originalcode:
   sub rsp,0x28
   xor edx,edx
-  
-  jmp 0x0032A866
-]], {"ntr_code"})
 
+  jmp 0x0032A866
+
+]], {"ntr_code"})
 
 print("No Teleport Requirement (NTR): Applying patch.")
 local readBytes = memory.readArray(codePointer, #originalBytes)
