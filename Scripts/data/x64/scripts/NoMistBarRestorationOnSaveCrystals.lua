a6f7a4ab121ad4d0c2b378510bbb8880aa64279b
Xeavin <64920094+Xeavin@users.noreply.github.com>
2025-11-24 16:40:02 +0100

    NMBROSC: Refactor script.

--- Scripts/data/x64/scripts/NoMistBarRestorationOnSaveCrystals.lua
+++ Scripts/data/x64/scripts/NoMistBarRestorationOnSaveCrystals.lua
@@ -3,29 +3,35 @@ local codePointer = 0x0030F483
 local originalBytes = {0xE8, 0x18, 0x01, 0x00, 0x00}
 local code =
 [[
-  jmp %nmbrosc_code%
+  call %nmbrosc_code%
 ]]
 
 local assemblies =
 [[
 nmbrosc_code:
-  test rdi,rdi
+  push rbx
+  push rdi
+  push rsi
+  sub rsp,0x20
+
+  test rcx,rcx
   je short nmbrosc_return
 
-  push rbx
-  push rsi
+  mov rdi,rcx
   movzx ebx,word ptr [rdi+0x06]
   movzx esi,byte ptr [rdi+0x4E]
-  sub rsp,0x18
+
   call 0x0030F5A0
-  add rsp,0x18
+
   mov [rdi+0x4E],sil
   mov [rdi+0x06],bx
-  pop rsi
-  pop rbx
 
 nmbrosc_return:
-  jmp 0x0030F488
+  add rsp,0x20
+  pop rsi
+  pop rdi
+  pop rbx
+  ret
 ]]
 
 local symbols = {
